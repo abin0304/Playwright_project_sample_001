fe89919925c926588744bdeff86dc42ada3198f2
abin0304 <abin03040@gmail.com>
2025-04-29 15:20:54 +0530

    updated playwright code

--- src/test/java/com/playwright/project001/simple_project_001/PlaywrightTest.java
+++ src/test/java/com/playwright/project001/simple_project_001/PlaywrightTest.java
@@ -2,23 +2,53 @@ package com.playwright.project001.simple_project_001;
 import com.microsoft.playwright.*;
 
 public class PlaywrightTest {
-    public static void main(String[] args) {
+    @SuppressWarnings("deprecation")
+	public static void main(String[] args) {
         // Create Playwright instance
-        try (Playwright playwright = Playwright.create()) {
+    	Playwright playwright = Playwright.create();
             // Launch Chrome browser
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(1000));
             
             // Open a new page
             Page page = browser.newPage();
             
-            // Navigate to Saucedemo website
             page.navigate("https://www.saucedemo.com/v1/");
+           
+            page.locator("//input[@id='user-name']").fill("standard_user");
             
-            // Print page title
+            page.locator("//input[@id='password']").fill("secret_sauce");
+            
+            page.locator("//input[@id='login-button']").click();
+            
+            page.waitForSelector("select.product_sort_container");
+            page.selectOption("select.product_sort_container", "lohi");
+            
+            page.click("//*[@id=\"inventory_container\"]/div/div[1]/div[3]/button");
+            
+            page.click("//a[@class='shopping_cart_link fa-layers fa-fw']//*[name()='svg']");
+            
+            System.out.println("Inside cart");
+            
+            page.click("//a[@class='btn_action checkout_button']");
+            
+            page.locator("//input[@id='first-name']").fill("User1234");
+            page.locator("//input[@id='last-name']").fill("name");
+            page.locator("//input[@id='postal-code']").fill("1221");
+            
+            page.click("//input[@value='CONTINUE']");
+            
+            page.locator("//a[@class='btn_action cart_button']").scrollIntoViewIfNeeded();
+            page.click("//a[@class='btn_action cart_button']");
+            
+            page.click("//button[normalize-space()='Open Menu']");
+            page.click("//a[@id='logout_sidebar_link']");
+            
+            System.out.println("success");
+              
+         // Print page title
             System.out.println("Page Title: " + page.title());
             
             // Close the browser
-            browser.close();
+           // browser.close();
         }
     }
-}
